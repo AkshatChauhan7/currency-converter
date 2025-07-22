734c2ee3dc9fa801b3bf4184d5de2e0cd2659526
AKSHAT CHAUHAN <chauhanakshat50@gmail.com>
2025-07-23 03:17:45 +0530

    Update postapi.jsx

--- src/api/postapi.jsx
+++ src/api/postapi.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+// https://v6.exchangerate-api.com/v6/f07645a0931923b3cdbf3e95/latest/USD
 const api = axios.create({
-    baseURL: 'your_api_key',
+    baseURL: 'https://v6.exchangerate-api.com/v6/' 
+,
 });
 
 export const getcurrency = (fromcurrency, tocurrency, amount) => {
