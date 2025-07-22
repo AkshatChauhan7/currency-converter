aa1a920f60f497f0170e7ca17b145f854710207f
AKSHAT CHAUHAN <chauhanakshat50@gmail.com>
2025-07-23 03:33:18 +0530

    Update postapi.jsx

--- src/api/postapi.jsx
+++ src/api/postapi.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // https://v6.exchangerate-api.com/v6/f07645a0931923b3cdbf3e95/latest/USD
 const api = axios.create({
-    baseURL: 'https://v6.exchangerate-api.com/v6/' 
+    baseURL: 'https://v6.exchangerate-api.com/v6/f07645a0931923b3cdbf3e95' 
 ,
 });
 
